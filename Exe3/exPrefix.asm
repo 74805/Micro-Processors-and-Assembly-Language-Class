1310077107614a0979de5cbd4343cf86afbb19f9
74805 <yuvtaub1308@gmail.com>
2023-05-27 19:25:38 +0300

    Added printDecimal

--- Exe3/exPrefix.asm
+++ Exe3/exPrefix.asm
@@ -9,9 +9,47 @@ proc numPrefix
 
 numPrefix endp
 
-proc Print
+printDecimal proc 
+    push ax
+    push bx
+    push cx
+    push dx
 
-Print endp
+    ; Check if the number is zero
+    mov cx, 0 ; Digit counter
+    mov bx, 10
+    cmp ax, 0
+    jne convertLoop
+    
+    mov dl, 30h
+    mov byte ptr es:[si], dl ; store in memory for printing
+    jmp printDecimalEnd
+
+    convertLoop:
+        xor dx, dx
+        div bx          ; Divide by 10
+        add dl, 30h     ; Convert remainder to ASCII
+        push dx         
+        inc cx          
+
+        cmp ax, 0
+    jnz convertLoop
+
+    printLoop:
+        pop dx          ; Pop digit from stack
+
+        mov byte ptr es:[si], dl ; store in memory for printing
+        add si, 2
+    loop printLoop
+
+    printDecimalEnd:
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret
+
+printDecimal endp
 
 START:
 	mov ax, @data ; Set up the data segment
@@ -19,7 +57,13 @@ START:
 
 	push 3257
 
-	call numPrefix
+    pop ax
+    ; set extra segment to screen
+	mov bx, 0B800h
+	mov es, bx
+	
+	mov si, 0
+	call printDecimal
 
 	; return to OS
 	mov ax, 4C00h

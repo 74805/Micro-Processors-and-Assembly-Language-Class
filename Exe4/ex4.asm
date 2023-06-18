c1004e26c0e7bdde7bb035a5ef4ef5ea094aad2c
74805 <yuvtaub1308@gmail.com>
2023-06-18 20:26:37 +0300

    Print symbol in the middle of the screen

--- Exe4/ex4.asm
+++ Exe4/ex4.asm
@@ -1,25 +1,47 @@
 .model small
 
 .data
+	rows db 25
+	columns db 80
 
 .stack 100h
 .code
 
 Print_Screen_Black proc uses ax bx cx dx
-    mov ah, 06h   ; Function to clear the screen and set attribute
+    mov ah, 06h ; Function to clear the screen and set attribute
     xor al, al
-    mov cx, 0     ; Starting row, column (0,0)
-    mov dx, 184Fh ; Ending row, column (79, 24)
-    mov bh, 0   ; Attribute for black background
+    mov cx, 0 ; Starting row, column (0,0)
 
-    int 10h       ; Call BIOS video service
+	; Set the ending row and column (24, 79)
+	mov dh, rows
+	mov dl, columns
+	dec dh
+	dec dl
+    mov bh, 0 ; Attribute for black background
+
+    int 10h ; Call BIOS video service
 
     ret
 Print_Screen_Black endp
 
-Print_Symbol proc
+Print_Symbol proc uses ax bx cx dx di
     ; Print red 'o' in the middle of the screen as the symbol
+    mov bx, 80 ; Number of columns
+    shr bx, 1 ; Divide by 2
+    mov ax, 25 ; Number of rows
+    shr ax, 1 ; Divide by 2 to get half of the rows
+	mov cx, 160 ; Number of bytes per row
+    mul cx 
+	
+    xor di, di
+	add di, ax ; Add the offset for half of the rows
+	add di, 80 ; Add the offset for half of the columns
 
+    ; Print 'O' with red foreground color on black background
+    mov byte ptr es:[di], 'O'
+    mov byte ptr es:[di + 1], 4h ; Attribute for red foreground color on black background
+
+    ret
 Print_Symbol endp
 
 START:
@@ -27,6 +49,10 @@ START:
 
     mov ax, @data ; Set up the data segment
 	mov ds, ax
+
+	; Set extra segment to screen
+	mov bx, 0B800h
+	mov es, bx
 
     call Print_Screen_Black
 

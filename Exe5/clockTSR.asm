88f8a029106f11f436ab1d76cadcf84c8f684be0
74805 <yuvtaub1308@gmail.com>
2023-06-27 01:19:41 +0300

    Print time

--- Exe5/clockTSR.asm
+++ Exe5/clockTSR.asm
@@ -1,13 +1,14 @@
 .model small
 
 .data
-    time db  0, ':', 0, ':', 0, '$'
+    time db  0, 0, 0
+    time_string db '00:00:00$'
 .stack 100h
 .code
 
 ; New_ISR resets the counter every third time the timer interrupt is called
 New_ISR proc uses si ax
-	mov si, offset time + 4 ; Point to the miliseconds
+	mov si, offset time + 2 ; Point to the miliseconds
     mov al, [si]
     mov ah, 0 
 
@@ -19,7 +20,7 @@ New_ISR proc uses si ax
         sub ax, 100 ; Subtract 100 miliseconds
         mov [si], al ; Save the miliseconds
 
-        mov si, offset time + 2 ; Point to the seconds
+        mov si, offset time + 1 ; Point to the seconds
         mov al, [si]
         
         add al, 1
@@ -35,6 +36,7 @@ New_ISR proc uses si ax
             add al, 1
 
 	Exit:
+        call Print_Time
 		iret
 New_ISR endp
 
@@ -61,6 +63,46 @@ Change_IVT proc uses ax es
 	ret
 Change_IVT endp
 
+; Print_Time prints the time to the screen
+Print_Time proc uses si ax bx di dx
+    ; Update the time string
+    mov si, offset time_string
+    mov di, offset time
+    mov bl, 10
+
+    mov al, [di]
+    mov ah, 0
+    div bl
+    add ax, 3030h
+    mov [si], ax
+
+    mov al, [di + 1]
+    mov ah, 0
+    div bl
+    add ax, 3030h
+    mov [si + 3], ax
+
+    mov al, [di + 2]
+    mov ah, 0
+    div bl
+    add ax, 3030h
+    mov [si + 6], ax
+
+    Set the cursor position
+    mov dh, 12
+    mov dl, 20
+    mov ah, 02h
+    mov bh, 0
+    int 10h
+
+    ; Print the time to the screen
+    mov dx, offset time_string
+    mov ah, 09h
+    int 21h
+
+    ret
+Print_Time endp
+
 START:
     .startup
 
@@ -68,6 +110,12 @@ START:
 	mov ds, ax
 
     call Change_IVT ; Update the timer interrupt vector
+
+    mov ax, 0b800h
+    mov es, ax
+    ;call Print_Time
 	
+    Loop1:
+        jmp Loop1
     .exit
 end START

6ee0b2dad88944725b98d5b589d256b08fb078a5
74805 <yuvtaub1308@gmail.com>
2023-06-26 19:31:50 +0300

    Backgrounf color chagne every 4 characters

--- Exe5/print.asm
+++ Exe5/print.asm
@@ -6,17 +6,18 @@
 .stack 100h
 .code
 
-; Prints the sentence, one character at a time, with a 1 second delay between each character
+; Prints the sentence, one character at a time, with a 1 second delay between each character, and background color changing every 4 characters
 Print_Sentence proc uses ax bx cx dx si
     ; Set up the cursor
     mov dh, 12
-    mov dl, 30
+    mov dl, 20
     mov ah, 02h
     mov bh, 0
     int 10h
 
     mov si, offset sentence
-    mov bl, 0Fh
+    mov dh, 0 ; counter
+    mov bl, 8h
     Loop1:
         ; Print a character
         mov al, [si]
@@ -28,62 +29,74 @@ Print_Sentence proc uses ax bx cx dx si
         int 10h
 
         inc si
+        inc dh
+        add di, 2
 
         ; Move cursor
         inc dl
+        push dx
+        mov dh, 12
         mov ah, 02h
         int 10h
+        pop dx
 
-        ; Wait 1 second
-        mov cx, 0FFFFh
-        Loop2:
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            push [si]
-            
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            pop [si]
-            loop Loop2
-            
+        ; Switch background color every 4 characters
+        cmp dh, 4
+        jne Wait_One_Second
+        add bl, 10h
+        and bl, 07Fh ; Make text not blink
+        mov dh, 0
+
+        Wait_One_Second:
+            mov cx, 0FFFFh
+            Loop2:
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                push [si]
+                
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                pop [si]
+                loop Loop2
+                
         jmp Loop1
 
     Print_Sentence_end:
